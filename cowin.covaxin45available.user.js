0368b8339b2a8ce8fb4810778f442f30507d6e41
Pankaj Batra <pankajb@gmail.com>
2021-05-09 12:25:56 +0530

    Create cowin.covaxin45available.user.js

---
+++ cowin.covaxin45available.user.js
@@ -0,0 +1,82 @@
+// ==UserScript==
+// @name         CoWin: Only show covaxin 45+ and bookable center records
+// @namespace    Improved on the version by jacobsingh
+// @version      0.2
+// @description  Only show covaxin 45+ and bookable records
+// @author       Pankaj Batra (github.com/pankajbatra)
+// @match        https://www.cowin.gov.in/*
+// @match        https://selfregistration.cowin.gov.in/appointment
+// @require      https://code.jquery.com/jquery-3.6.0.min.js
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    // Probably could remove this, but it makes the element mutation finder code easier.
+    var $ = jQuery;
+    var mCoinSound = new Audio("http://soundbible.com/grab.php?id=2206&type=mp3");
+
+    function onElementInserted(containerSelector, elementSelector, callback) {
+
+        var onMutationsObserved = function(mutations) {
+            mutations.forEach(function(mutation) {
+                if (mutation.addedNodes.length) {
+                    var elements = $(mutation.addedNodes).find(elementSelector);
+                    for (var i = 0, len = elements.length; i < len; i++) {
+                        callback(elements[i]);
+                    }
+                }
+            });
+        };
+
+        var target = $(containerSelector)[0];
+        var config = { childList: true, subtree: true };
+        var MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+        var observer = new MutationObserver(onMutationsObserved);
+        observer.observe(target, config);
+
+    }
+
+    onElementInserted('body', '.center-box', function(element) {
+        console.log("center Box");
+        setInterval(function() {
+            mCoinSound.pause();
+            mCoinSound.currentTime = 0;
+            $(".pin-search-btn").click(); }, 15*1000);
+    });
+
+    onElementInserted('body', '.mat-list-text', function(element) {
+        var slotFound = false;
+        var is18Plus = false;
+         $(element).find( "a[href$='/appointment']" ).each(function(i, linkObj) {
+             var ageElem = $(linkObj).siblings('div.ng-star-inserted')[0]
+             //.children('span.age-limit')
+             if (ageElem && ageElem.innerText != "Age 45+") {
+                 if(linkObj.closest('div.row')) {
+                     linkObj.closest('div.row').style.display = "none";
+                 }
+                 // for the authenticated list which uses different HTML
+                 linkObj.closest('div.mat-list-item-content').parentNode.style.display = "none";
+                 console.log(" No 45+ Slot in : "+$(element).find('h5.center-name-title')[0].innerText);
+                 is18Plus = true;
+                 return;
+             }
+             var vaccineName = $(linkObj).siblings('div.vaccine-cnt')[0];
+             if (vaccineName && vaccineName.innerText.toUpperCase() == "COVAXIN"){
+                 if (!linkObj.innerText.includes("NA") && !linkObj.innerText.includes("Booked")) {
+                     slotFound = true;
+                 }
+             }
+         });
+        if(!slotFound && !is18Plus){
+            console.log(" No available Slot in : "+$(element).find('h5.center-name-title')[0].innerText);
+            element.style.display = "none";
+        }
+        else if(!is18Plus){
+            console.log("Slot found in : "+$(element).find('h5.center-name-title')[0].innerText);
+            mCoinSound.play();
+        }
+    });
+
+
+})();
