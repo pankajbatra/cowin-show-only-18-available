17f40c9fe790cf7c390e39d955d5cbfc16c644bc
Pankaj Batra <pankajb@gmail.com>
2021-05-10 01:42:52 +0530

    roll back

--- cowin.45available.user.js
+++ cowin.45available.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name         CoWin: Only show 18+ and bookable center records
+// @name         CoWin: Only show 45+ and bookable center records
 // @namespace    Improved on the version by jacobsingh
 // @version      0.2
-// @description  Only show 18+ and bookable records
+// @description  Only show 45+ and bookable records
 // @author       Pankaj Batra (github.com/pankajbatra)
 // @match        https://www.cowin.gov.in/*
 // @match https://selfregistration.cowin.gov.in
@@ -96,28 +96,29 @@ var $ = jQuery;
 
     onElementInserted('body', '.mat-list-text', function(element) {
         var slotFound = false;
-        var is45Plus = false;
+        var is18Plus = false;
          $(element).find( "a[href$='/appointment']" ).each(function(i, linkObj) {
-             var ageElem = $(linkObj).siblings('div.ng-star-inserted')[0];
-             if (ageElem && ageElem.innerText != "Age 18+") {
+             var ageElem = $(linkObj).siblings('div.ng-star-inserted')[0]
+             //.children('span.age-limit')
+             if (ageElem && ageElem.innerText != "Age 45+") {
                  if(linkObj.closest('div.row')) {
                      linkObj.closest('div.row').style.display = "none";
                  }
                  // for the authenticated list which uses different HTML
                  linkObj.closest('div.mat-list-item-content').parentNode.style.display = "none";
-                 console.log(" No 18+ Slot in : "+$(element).find('h5.center-name-title')[0].innerText);
-                 is45Plus = true;
+                 console.log(" No 45+ Slot in : "+$(element).find('h5.center-name-title')[0].innerText);
+                 is18Plus = true;
                  return;
              }
              if (!linkObj.innerText.includes("NA") && !linkObj.innerText.includes("Booked")) {
                  slotFound = true;
              }
          });
-        if(!slotFound && !is45Plus){
+        if(!slotFound && !is18Plus){
             console.log(" No available Slot in : "+$(element).find('h5.center-name-title')[0].innerText);
             element.style.display = "none";
         }
-        else if(!is45Plus){
+        else if(!is18Plus){
             console.log("Slot found in : "+$(element).find('h5.center-name-title')[0].innerText);
             mCoinSound.play();
         }
