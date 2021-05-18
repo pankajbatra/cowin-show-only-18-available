2833e456cd1c3757fab8dd3b5ce4eb5732ef4023
Pankaj Batra <pankajb@gmail.com>
2021-05-18 18:35:05 +0530

    fix for multiple age group in same row

--- cowin.covaxin18available.user.js
+++ cowin.covaxin18available.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         CoWin: Only show covaxin - 18+ and bookable center records
 // @namespace    Improved on the version by jacobsingh
-// @version      0.5
+// @version      0.6
 // @description  Only show covaxin 18+ and bookable records
 // @author       Pankaj Batra (github.com/pankajbatra)
 // @downloadURL  https://github.com/pankajbatra/cowin-show-only-18-available/raw/main/cowin.covaxin18available.user.js
@@ -107,31 +107,22 @@ var $ = jQuery;
 
     onElementInserted('body', '.mat-list-text', function(element) {
         var slotFound = false;
-        var is45Plus = false;
          $(element).find( "a[href$='/appointment']" ).each(function(i, linkObj) {
              var ageElem = $(linkObj).siblings('div.ng-star-inserted')[0];
-             if (ageElem && ageElem.innerText != "Age 18+") {
-                 if(linkObj.closest('div.row')) {
-                     linkObj.closest('div.row').style.display = "none";
-                 }
-                 // for the authenticated list which uses different HTML
-                 linkObj.closest('div.mat-list-item-content').parentNode.style.display = "none";
-                 //console.log(" No 18+ Slot in : "+$(element).find('h5.center-name-title')[0].innerText);
-                 is45Plus = true;
-                 return;
-             }
-             var vaccineName = $(linkObj).siblings('div.vaccine-cnt')[0];
-             if (vaccineName && vaccineName.innerText.toUpperCase() == "COVAXIN"){
-                 if (!linkObj.innerText.includes("NA") && !linkObj.innerText.includes("Booked")) {
-                     slotFound = true;
+             if (ageElem && ageElem.innerText == "Age 18+"){
+                 var vaccineName = $(linkObj).siblings('div.vaccine-cnt')[0];
+                 if (vaccineName && vaccineName.innerText.toUpperCase() == "COVAXIN"){
+                     if (!linkObj.innerText.includes("NA") && !linkObj.innerText.includes("Booked")) {
+                         slotFound = true;
+                     }
                  }
              }
          });
-        if(!slotFound && !is45Plus){
+        if(!slotFound){
             //console.log(" No available Slot in : "+$(element).find('h5.center-name-title')[0].innerText);
             element.style.display = "none";
         }
-        else if(!is45Plus){
+        else{
             console.log("Slot found in : "+$(element).find('h5.center-name-title')[0].innerText);
             mCoinSound.play();
         }
